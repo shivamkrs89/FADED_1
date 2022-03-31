b82655c0dca65633387eb45e0c9b92a910df22ea
shivam kumar <imshivamkr2@gmail.com>
2022-03-31 19:18:34 +0530

    bugs removed

--- common/connect.py
+++ common/connect.py
@@ -6,10 +6,10 @@ from datetime import datetime
 def connect():
     try:
         mydb = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='shivam@123',
-            database='codemate',
+            host='us-cdbr-east-05.cleardb.net',
+            user='b70b5b318003ac',
+            password='356f8baa',
+            database='heroku_af5ccba6c9a4d11',
         )
     except:
         connect()
